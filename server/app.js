f99c47b068756d06094343939fcdc8920e464870
Thibb1 <thibault.brumaire@gmail.com>
2022-09-28 12:13:22 +0200

    + about.json

--- server/app.js
+++ server/app.js
@@ -6,6 +6,36 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+function getServices() {
+    // fetch DB return available services
+    // {
+    //     "name": "Facebook",
+    //     "actions": [{
+    //       "name": "action_1",
+    //       "description": "action_dsc_1"
+    //     }],
+    //     "reactions": [{
+    //       "name": "reaction_1",
+    //       "description": "reaction_dsc_1"
+    //     }]
+    // }
+    return []
+}
+
+app.get('/about.json', (req, res) => {
+    const ip = req.ip.split(':')[3]
+    console.log(ip)
+    res.json({
+        "client": {
+            "host": ip
+        },
+        "server": {
+            "current_time": Date.now()
+        },
+        "services": getServices()
+    })
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
