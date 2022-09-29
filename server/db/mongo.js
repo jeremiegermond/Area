4320a3b32f9ec7be20c2ec201caaa9c54d0aa82b
Thibb1 <thibault.brumaire@gmail.com>
2022-09-29 12:16:04 +0200

    + user add del get and update

--- server/db/mongo.js
+++ server/db/mongo.js
@@ -6,7 +6,7 @@ const clientOptions = {
 
 exports.initDbConnection = async () => {
     try {
-        await mongoose.connect(process.env.URL_MONGO || 'mongodb://user_area:pass_area@mongo')
+        await mongoose.connect(process.env.URL_MONGO || 'mongodb://user_area:pass_area@mongo', clientOptions)
         console.log('db connected')
     } catch (e) {
         console.log(e)
--- server/routes/v1/user.js
+++ server/routes/v1/user.js
@@ -5,4 +5,10 @@ const service = require('../../services/v1/user')
 
 router.get('/:id', service.getById)
 
+router.put('/add', service.add)
+
+router.patch('/update', service.update)
+
+router.delete('/delete', service.delete)
+
 module.exports = router
