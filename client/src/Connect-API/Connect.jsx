3d1f33bc645b783334bf98982cccd831071ab23c
Jérémie Germond <jeremie.germond@epitech.eu>
2022-09-30 18:32:46 +0200

    [+] adding beggining of connect page

--- client/src/Connect-API/Connect.jsx
+++ client/src/Connect-API/Connect.jsx
@@ -2,9 +2,15 @@ import './Connect.css';
 
 function Connect() {
   return (
-    <div className="home">
-      <p>hello</p>
-    </div>
+    <section className='connect-page'>
+      <h1>Connect to your API's</h1>
+      <h3>By doing this, you consent the usage of your data</h3>
+      <div className='api-buttons'>
+          <input type="submit" value='Google' className='api-btn' />
+          <input type="submit" value='Facebook' className='api-btn' />
+          <input type="submit" value='Twitter' className='api-btn' />
+      </div>
+    </section>
   );
 }
 
