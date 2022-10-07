060f5a1803df6140a027d9521a6a461768138b36
Jeremie <jeremie@localhost.localdomain>
2022-10-07 19:28:03 +0200

    [+] adding home route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Register from './Register/Register'
 import Login from './Login/Login'
 import ConnectAPI from './Connect-API/Connect';
+import Home from './Home/Home'
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route exact path="/register" element={<Register/>}/>
         <Route exact path="/login" element={<Login/>}/>
         <Route exact path="/connect-api" element={<ConnectAPI/>}/>
+        <Route exact path="/home" element={<Home/>}/>
       </Routes>
     </Router>
   );
