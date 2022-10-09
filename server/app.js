66aa6b59e82e7d7b676d9849260e0099e3dfcaee
Thibb1 <thibault.brumaire@gmail.com>
2022-10-09 15:06:35 +0200

    * swapped while loop to setInterval

--- server/app.js
+++ server/app.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const passport = require('passport');
-const bodyParser = require('body-parser');
-const mongodb = require('./db/mongo')
-const cookieParser = require('cookie-parser')
-const logger = require('morgan')
-const cors = require('cors')
-const Users = require('./models/v1/user')
-const Action = require('./models/v1/action')
-const Reaction = require('./models/v1/reaction')
+const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const bodyParser = require("body-parser");
+const mongodb = require("./db/mongo");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+const Users = require("./models/v1/user");
+const Action = require("./models/v1/action");
+const Reaction = require("./models/v1/reaction");
 //const https = require('https');
 //const fs = require('fs');
 
@@ -17,64 +17,59 @@ const Reaction = require('./models/v1/reaction')
   cert: fs.readFileSync('cert.pem')
 }; */
 
-require('./auth/auth');
+require("./auth/auth");
 
-const port = process.env.PORT || 8080
-mongodb.initDbConnection()
+const port = process.env.PORT || 8080;
+mongodb.initDbConnection();
 
-const routes = require('./routes/v1/index');
-const secureRoute = require('./routes/v1/secure-index');
+const routes = require("./routes/v1/index");
+const secureRoute = require("./routes/v1/secure-index");
 
 const app = express();
 app.use(passport.initialize());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser())
-app.use(logger('dev'))
-app.use(express.json()) 
+app.use(cookieParser());
+app.use(logger("dev"));
+app.use(express.json());
 
-app.use(cors({
-  credentials: true,
-}))
+app.use(
+  cors({
+    credentials: true,
+  })
+);
 
-app.use('/', routes);
+app.use("/", routes);
 
 // Plug in the JWT strategy as a middleware so only verified users can access this route.
-app.use('/user', passport.authenticate('jwt', { session: false }), secureRoute);
-
-// Handle errors.
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({ error: err });
-});
+app.use("/user", passport.authenticate("jwt", { session: false }), secureRoute);
 
 app.listen(port);
 
-//https.createServer(app).listen(port);
-console.log(`http listening on port ${port}`)
+console.log(`Server listening on port ${port}`);
 
-async function loop() {
-  while(true) {
-    Users.find({} , (err, users) => {
-      if(err)
-        console.log(err)
-      users.map(user => {
+function checkActions() {
+  try {
+    Users.find({}, (err, users) => {
+      if (err) {
+        console.log("found err");
+        console.log(err);
+      }
+      users.forEach((user) => {
+        // console.log("Got user");
+        // console.log(user.username);
         try {
-          console.log(user.username)
-          user.populate("actionReaction.action").then(() => {
-            user.actionReaction.map(ar => {
-              console.log(ar.action)
-              //ar.action.check({
-              //  headers: {
-              //    authorization: `Bearer AAAAAAAAAAAAAAAAAAAAACZZhwEAAAAAjmB7uIqIMiuPd%2FqsJbgdLBIgfUw%3DcqQUx6IYf5oGhYndh4KO9ePZtZgUtRsqRjaUdw5HDL1ECkRF1p`
-              //  }})
-            })
-          })
-        } catch(error) {
-        console.log(error)
+          user.actionReaction.forEach((ar) => {
+            console.log(ar);
+          });
+        } catch (e) {
+          console.log("catch error ar");
+          console.log(e);
         }
-      })
-    })
-    await new Promise(r => setTimeout(r, 5000));
+      });
+    });
+  } catch (e) {
+    console.log("catch error");
+    console.log(e);
   }
 }
-loop()
+setInterval(checkActions, 5 * 1000);
