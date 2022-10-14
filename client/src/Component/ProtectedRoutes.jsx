8fc6c7b45d1360e49a5683f7075395389c5a2be1
Thibb1 <thibault.brumaire@gmail.com>
2022-10-14 14:10:24 +0200

    * fix ProtectedRoutes.jsx

--- client/src/Component/ProtectedRoutes.jsx
+++ client/src/Component/ProtectedRoutes.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Outlet } from "react-router-dom";
 
-export default function ProtectedRoutes() {
-    const auth = null;
-    return auth ? <Outlet /> : <Navigate to="/home" />;
+export default function ProtectedRoutes(props) {
+  const [auth, setAuth] = useState("");
+  useEffect(() => {
+    // cookies
+    // setAuth(getlocalstorage @auth);
+  }, []);
+
+  return auth.length <= 0 ? <Outlet /> : <>{props.children}</>;
 }
--- client/src/Register/Register.jsx
+++ client/src/Register/Register.jsx
@@ -1,8 +1,8 @@
-import './Register.css';
-import { FaTwitter, FaGoogle, FaFacebook } from 'react-icons/fa';
-import React, { useState } from 'react';
-import { Form, Button } from "react-bootstrap";
-import axios from 'axios';
+import "./Register.css";
+import { FaFacebook, FaGoogle, FaTwitter } from "react-icons/fa";
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import axios from "axios";
 
 export default function Register() {
   const [username, setUser] = useState("");
@@ -10,57 +10,84 @@ export default function Register() {
   const [register, setRegister] = useState(false);
   const client = {
     username,
-    password
-  }
+    password,
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(client);
-    axios.post('http://localhost:8080/signup', client)
-    .then((res) => {
-      setRegister(true);
-      console.log(res);
-      console.log(res.data);
-    })
-    .catch((error) => {
-      error = new Error();
-    });
-  }
+    axios
+      .post("http://localhost:8080/signup", client)
+      .then((res) => {
+        setRegister(true);
+        // savetolocalstorage @auth
+        console.log(res);
+        console.log(res.data);
+      })
+      .catch((error) => {
+        error = new Error();
+      });
+  };
 
   return (
     <>
       <div className="home">
-        <div className='register-box'>
-          <h1 className='title'>Register</h1>
-          <Form onSubmit={(e)=>handleSubmit(e)}>
-            <div className='register-box-btns'>
+        <div className="register-box">
+          <h1 className="title">Register</h1>
+          <Form onSubmit={(e) => handleSubmit(e)}>
+            <div className="register-box-btns">
               <Form.Group controlId="formUsername">
-                <label htmlFor='username'></label>
-                <input value={username} type='text' name='username' placeholder='Username' className='btn' onChange={(e) => setUser(e.target.value)} required/>
+                <label htmlFor="username"></label>
+                <input
+                  value={username}
+                  type="text"
+                  name="username"
+                  placeholder="Username"
+                  className="btn"
+                  onChange={(e) => setUser(e.target.value)}
+                  required
+                />
               </Form.Group>
               <Form.Group controlId="formPassword">
-                <label htmlFor='password'></label>
-                <input value={password} type='password' name='password' placeholder='Password' className='btn' onChange={(e) => setPassword(e.target.value)} required/>
+                <label htmlFor="password"></label>
+                <input
+                  value={password}
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  className="btn"
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
               </Form.Group>
-              <div className='separator'>
-                <div className='line'></div> 
-                <p> or </p> 
-                <div className='line'></div> 
+              <div className="separator">
+                <div className="line"></div>
+                <p> or </p>
+                <div className="line"></div>
               </div>
-              <div className='register-box-icons'>
-                <FaTwitter className='icon' />
-                <FaGoogle className='icon' />
-                <FaFacebook className='icon' />
+              <div className="register-box-icons">
+                <FaTwitter className="icon" />
+                <FaGoogle className="icon" />
+                <FaFacebook className="icon" />
               </div>
             </div>
-            <Button variant="primary" type="submit" className='login-btn' onClick={(e) => handleSubmit(e)}>Register</Button>
+            <Button
+              variant="primary"
+              type="submit"
+              className="login-btn"
+              onClick={(e) => handleSubmit(e)}
+            >
+              Register
+            </Button>
             {register ? (
               <p className="text-success">You are successfully registered</p>
             ) : (
               <p className="text-danger">You are not registered</p>
             )}
           </Form>
-          <p>You already have an account ? Go to <a href="../Login">login</a></p>
+          <p>
+            You already have an account ? Go to <a href="../Login">login</a>
+          </p>
         </div>
       </div>
     </>
