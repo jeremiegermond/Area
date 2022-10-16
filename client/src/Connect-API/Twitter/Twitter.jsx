3584bb244638f6de1c36431dd5a055c7d773606a
Thibb1 <thibault.brumaire@gmail.com>
2022-10-16 19:55:43 +0200

    * fix Twitter.jsx missing import

--- client/src/Connect-API/Twitter/Twitter.jsx
+++ client/src/Connect-API/Twitter/Twitter.jsx
@@ -1,33 +1,36 @@
-import React from 'react';
-import axios from 'axios';
-import queryString from 'query-string';
+import React from "react";
+import axios from "axios";
+import queryString from "query-string";
+import Cookies from "universal-cookie";
 
 const cookies = new Cookies();
-const auth = cookies.get("TOKEN")
+const auth = cookies.get("TOKEN");
 
 export default class Twitter extends React.Component {
-
-    componentDidMount() {
-        const {oauth_token, oauth_verifier} = queryString.parse(window.location.search);
-        if (oauth_token && oauth_verifier) {
-          try {
-            axios({
-              method: 'post',
-              url: 'http://localhost:8080/user/twitter/callback',
-              headers: {"Authorization" : `Bearer ${auth}`},
-              data: {
-                oauth_token, oauth_verifier
-              }
-            })
-          } catch (error) {
-           console.error(error); 
-          }
-        window.location.href = "http://localhost:8081/connect-api/"
-        }
+  componentDidMount() {
+    const { oauth_token, oauth_verifier } = queryString.parse(
+      window.location.search
+    );
+    if (oauth_token && oauth_verifier) {
+      try {
+        axios({
+          method: "post",
+          url: "http://localhost:8080/user/twitter/callback",
+          headers: { Authorization: `Bearer ${auth}` },
+          data: {
+            oauth_token,
+            oauth_verifier,
+          },
+        });
+      } catch (error) {
+        console.error(error);
       }
-      render() {
-        return null
-      }
+      window.location.href = "http://localhost:8081/connect-api/";
+    }
+  }
+  render() {
+    return null;
+  }
 }
 
 /*
