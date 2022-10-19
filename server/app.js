ba2c5ec6dca2f16c5bf194c1921a9c2737a11646
Thibb1 <thibault.brumaire@gmail.com>
2022-10-19 21:42:24 +0200

    + https via env on server

--- server/app.js
+++ server/app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const passport = require("passport");
 const bodyParser = require("body-parser");
 const mongodb = require("./db/mongo");
@@ -7,15 +6,8 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
 const Users = require("./models/v1/user");
-const Action = require("./models/v1/action");
-const Reaction = require("./models/v1/reaction");
-//const https = require('https');
-//const fs = require('fs');
-
-/* const options = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem')
-}; */
+const https = require("https");
+const fs = require("fs");
 
 require("./auth/auth");
 
@@ -43,10 +35,18 @@ app.use("/", routes);
 // Plug in the JWT strategy as a middleware so only verified users can access this route.
 app.use("/user", passport.authenticate("jwt", { session: false }), secureRoute);
 
-app.listen(port);
+if (process.env.HTTPS === true) {
+  const credentials = {
+    key: fs.readFileSync((path = process.env.SSL_KEY_FILE)),
+    cert: fs.readFileSync((path = process.env.SSL_CRT_FILE)),
+  };
+  const httpsServer = https.createServer(credentials, app);
+  httpsServer.listen(port);
+} else {
+  app.listen(port);
+}
 
 console.log(`Server listening on port ${port}`);
-
 
 function checkActions() {
   try {
@@ -59,14 +59,14 @@ function checkActions() {
         try {
           user.populate("actionReaction.action").then((u) => {
             u.actionReaction.map(async (ar) => {
-              if (await ar.action.check(u) == true)
+              if ((await ar.action.check(u)) === true)
                 user.populate("actionReaction.reaction").then(async () => {
-                  await ar.reaction.exec(u)
-               })
-            })
-          })
-        } catch(error) {
-        console.log(error)
+                  await ar.reaction.exec(u);
+                });
+            });
+          });
+        } catch (error) {
+          console.log(error);
         }
       });
     });
