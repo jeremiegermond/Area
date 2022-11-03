10e150fa2301cea45735053eeee9a735b0124812
Thibb1 <thibault.brumaire@gmail.com>
2022-11-03 21:58:57 +0100

    [+] options can be set when adding action/reaction on mobile client

--- client-app/src/screens/add/index.js
+++ client-app/src/screens/add/index.js
@@ -8,16 +8,23 @@ import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {getServer, postServer} from '../../api';
 import PressableIcon from '../../components/PressableIcon';
 import {faLink} from '@fortawesome/free-solid-svg-icons';
+import OptionModal from './OptionModal';
 
 const AddScreen = ({navigation}) => {
   const [actionId, setActionId] = useState('');
+  const [actionParams, setActionParams] = useState([]);
   const [actionList, setActionList] = useState([]);
   const [reactionList, setReactionList] = useState([]);
   const [currentList, setCurrentList] = useState([]);
+  const [modal, setModal] = useState(false);
+  const [options, setOptions] = useState([]);
+  const [currentItem, setCurrentItem] = useState(undefined);
+
   useEffect(() => {
     const unsubscribe = navigation.addListener('focus', () => {
       setCurrentList(actionList);
       setActionId('');
+      setActionParams('');
       getServer('user/getActions')
         .then(r => {
           setActionList(r.data);
@@ -32,9 +39,62 @@ const AddScreen = ({navigation}) => {
     });
     return unsubscribe;
   }, [navigation]);
+
+  function handleItemClick(item) {
+    if (item.options.length > 0) {
+      setCurrentItem(item);
+      setOptions(
+        item.options.map(option => {
+          return {
+            ...option,
+            value: '',
+          };
+        }),
+      );
+      setModal(true);
+    } else {
+      addItem(item, '');
+    }
+  }
+  function addItem(item, params) {
+    if (actionId.length > 0) {
+      postServer('user/addActionReaction', {
+        action_id: actionId,
+        reaction_id: item._id,
+        action_params: actionParams,
+        reaction_params: params,
+      })
+        .then(() => {
+          navigation.navigate('Home');
+        })
+        .catch(e => console.log(e));
+    } else {
+      setActionId(item._id);
+      setActionParams(params);
+      setCurrentList(reactionList);
+    }
+  }
+  function submitModal() {
+    const params = options
+      .map(e => {
+        return `${e.name}:${e.value}`;
+      })
+      .join();
+    addItem(currentItem, params);
+    setModal(false);
+  }
+
   return (
     <>
       <DefaultView>
+        <OptionModal
+          visible={modal}
+          onCancel={() => setModal(false)}
+          currentItem={currentItem}
+          options={options}
+          onSetOptions={setOptions}
+          onSubmit={submitModal}
+        />
         <Text style={styles.mainText}>
           {actionId.length > 0 ? 'Do___' : 'When___'}
         </Text>
@@ -47,37 +107,23 @@ const AddScreen = ({navigation}) => {
                   height={'100%'}
                   radius={40}
                   onPress={() => {
-                    if (actionId.length > 0) {
-                      postServer('user/addActionReaction', {
-                        action_id: actionId,
-                        reaction_id: item._id,
-                      })
-                        .then(() => {
-                          navigation.navigate('Home');
-                        })
-                        .catch(e => console.log(e));
-                    } else {
-                      setActionId(item._id);
-                      setCurrentList(reactionList);
-                    }
+                    handleItemClick(item);
                   }}>
                   <FontAwesomeIcon
                     icon={icons[item.service.name]}
                     size={50}
-                    style={{width: '30%'}}
+                    style={styles.arIcon}
                   />
-                  <Text style={{width: '70%', color: 'black', fontSize: 30}}>
-                    {item.name}
-                  </Text>
+                  <Text style={styles.arText}>{item.name}</Text>
                 </DefaultPressable>
               </DefaultBox>
             );
           })}
-          <DefaultBox padding={0} style={{backgroundColor: 'lightgreen'}}>
+          <DefaultBox padding={0} style={styles.greenBg}>
             <PressableIcon
               icon={faLink}
               size={80}
-              style={{width: '100%', height: '100%', borderRadius: 40}}
+              style={styles.connectApiBox}
               onPress={() => navigation.navigate('Profile')}
             />
           </DefaultBox>
@@ -98,29 +144,16 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     flexWrap: 'wrap',
   },
-  boxTextView: {
-    width: '70%',
-    justifyContent: 'space-around',
-  },
-  boxIconView: {
-    maxWidth: '30%',
-    justifyContent: 'space-evenly',
-  },
   mainText: {
     textAlignVertical: 'center',
     height: 70,
     color: 'black',
     fontSize: 30,
   },
-  boxText: {
-    color: 'black',
-    fontSize: 20,
-  },
-  boxBoldText: {
-    color: 'black',
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
+  arIcon: {flex: 1, margin: 10},
+  arText: {flex: 3, color: 'black', fontSize: 30},
+  greenBg: {backgroundColor: 'lightgreen'},
+  connectApiBox: {width: '100%', height: '100%', borderRadius: 40},
 });
 
 export default AddScreen;
